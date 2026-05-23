a44fbe0a8d2c283545e8c67ce30889c96f7ae95f
daijoubu <daijoubu@gmail.com>
2026-05-23 07:07:41 -0700

    dronecan/h7: reduce FDCAN SJW from 8 to 3
    SJW=8 was overly conservative (80% of bit time at 1Mbps with 10 quanta).
    SJW=3 is the standard value also used by the F7 driver.
    
    Tested with 6037 arm/disarm cycles at 500kbps: TEC=0, REC=0, zero errors.

--- src/main/drivers/dronecan/libcanard/canard_stm32h7xx_driver.c
+++ src/main/drivers/dronecan/libcanard/canard_stm32h7xx_driver.c
@@ -181,7 +181,7 @@ static bool canardSTM32ComputeTimings(const uint32_t target_bitrate, struct Timi
           (int)(1 + solution.bs1 + solution.bs2), (double)(solution.sample_point_permill) / (double)(10.0));
 
     out_timings->prescaler = (uint16_t)(prescaler);
-    out_timings->sjw = 8;                        // Conservative SJW to maximize resync with SLCAN adapters
+    out_timings->sjw = 3;                        // Standard SJW
     out_timings->bs1 = (uint8_t)(solution.bs1);  // The HAL takes care of the 1 bs offset in the register so don't remove it here like AP does.
     out_timings->bs2 = (uint8_t)(solution.bs2);  // The HAL takes care of the 1 bs offset in the register so don't remove it here like AP does.
 
